876ae2a240f1d0d510a9c87693652f495180f464
Eric Douglass <eric.douglass@gmail.com>
2022-03-09 09:53:20 -0800

    Added more cors

--- webclient/src/screens/HomeScreen/index.js
+++ webclient/src/screens/HomeScreen/index.js
@@ -123,11 +123,20 @@ export default function HomeScreen({ appState, setAppState }) {
         setActivityLogPage(0);
     }, []);
 
+    function serverTest() {
+        axios.post('/server/test', {})
+            .then(res => {
+                console.log(`Server response: `, res);
+            })
+            .catch(err => console.log(`Server response ERROR: `, err));
+    }
+
 
     return (
         <div id="appScreen" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '0.5rem'}}>
             <div style={{display: 'flex', width: '100%', justifyContent: 'flex-end'}}>
                 <button style={{}} onClick={logout}>Log Out</button>
+                <button onClick={serverTest}>Server Boop</button>
             </div>
             
             <div style={{display: 'flex'}}>
