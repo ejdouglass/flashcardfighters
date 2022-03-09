9b438c20cd822a39c4cfa81897b96995fea8d0b9
Eric Douglass <eric.douglass@gmail.com>
2022-03-09 09:58:38 -0800

    Quick cors test

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const User = require('./models/User');
@@ -93,11 +94,14 @@ mongoose.connect(process.env.DB_HOST)
     .then(() => console.log(`Successfully connected to Flashcard Fighters database.`))
     .catch(err => console.log(`Error connecting to Flashcard Fighters database: ${err}`));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+app.use(cors({
+    origin: 'http://localhost:3456'
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
